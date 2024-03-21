d01c201fb6572097730077f3a69bb0284a2845da
Juanfran <juanfran.ag@gmail.com>
2024-03-21 17:41:25 +0100

    fix: real time comments

--- libs/nodes/src/lib/comments/comments.store.ts
+++ libs/nodes/src/lib/comments/comments.store.ts
@@ -1,35 +1,45 @@
-import { inject } from '@angular/core';
-import { patchState, signalStore, withMethods, withState } from '@ngrx/signals';
+import { computed, inject } from '@angular/core';
+import {
+  patchState,
+  signalStore,
+  withComputed,
+  withMethods,
+  withState,
+} from '@ngrx/signals';
 import { CommentNode, TuNode } from '@team-up/board-commons';
-
 import { NodesStore } from '../services/nodes.store';
 
 type CommentsState = {
   parentNodeId: TuNode['id'] | undefined;
-  comments: CommentNode[];
 };
 
 const initialState: CommentsState = {
   parentNodeId: undefined,
-  comments: [],
 };
 
 export const CommentsStore = signalStore(
   { providedIn: 'root' },
   withState(initialState),
-  withMethods((store, nodesStore = inject(NodesStore)) => ({
+  withComputed(({ parentNodeId }, nodesStore = inject(NodesStore)) => ({
+    comments: computed(() => {
+      const node = nodesStore.nodes().find((it) => it.id === parentNodeId());
+
+      if (!node) {
+        return [];
+      }
+
+      return (
+        node?.children?.filter(
+          (it): it is CommentNode => it.type === 'comment',
+        ) ?? []
+      );
+    }),
+  })),
+  withMethods((store) => ({
     setParentNode: (parentNodeId: string) => {
-      const node = nodesStore.nodes().find((it) => it.id === parentNodeId);
-
-      if (node) {
-        const comments =
-          node.children?.filter(
-            (it): it is CommentNode => it.type === 'comment',
-          ) ?? [];
-
+      if (parentNodeId) {
         patchState(store, {
           parentNodeId,
-          comments,
         });
       } else {
         patchState(store, initialState);
