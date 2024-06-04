581881bad36c6830592530cf3bc1bb21e1efb30e
Juanfran <juanfran.ag@gmail.com>
2024-06-04 16:10:14 +0200

    feat: sync config load

--- generate-config.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-API_URL="${API_URL}"
-WS_URL="${WS_URL}"
-
-echo "{\"API_URL\":\"$API_URL\",\"WS_URL\":\"$WS_URL\"}" > /usr/share/nginx/html/assets/config.json
-
-nginx -g 'daemon off;'
