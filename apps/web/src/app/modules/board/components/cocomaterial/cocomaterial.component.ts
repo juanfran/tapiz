9c626851877d5d327922576a6a21d5bf9a9d6d63
Juanfran <juanfran.ag@gmail.com>
2024-07-23 16:53:50 +0200

    feat: new cocomaterial selector

--- apps/web/src/app/modules/board/components/cocomaterial/cocomaterial.component.ts
+++ apps/web/src/app/modules/board/components/cocomaterial/cocomaterial.component.ts
@@ -1,4 +1,12 @@
-import { Component, ElementRef, ViewChild, inject } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Signal,
+  viewChild,
+  computed,
+  inject,
+  signal,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
@@ -11,30 +19,25 @@ import {
 } from '@angular/material/autocomplete';
 import { COMMA, ENTER } from '@angular/cdk/keycodes';
 import { RxState } from '@rx-angular/state';
-import { CocomaterialTag, CocomaterialApiVector } from '@tapiz/board-commons';
-import { Store } from '@ngrx/store';
 import {
-  selectCocomaterial,
-  selectPosition,
-  selectZoom,
-} from '../../selectors/page.selectors';
-import { map, startWith, take, withLatestFrom } from 'rxjs';
+  CocomaterialTag,
+  CocomaterialApiVector,
+  Point,
+} from '@tapiz/board-commons';
+import { Store } from '@ngrx/store';
+import { selectCocomaterial } from '../../selectors/page.selectors';
+import { filter, map, startWith } from 'rxjs';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { PageActions } from '../../actions/page.actions';
 import { SvgBackgroundDirective } from '../../directives/svg-background.directive';
 import { InfiniteScrollDirective } from 'ngx-infinite-scroll';
 import { SafeHtmlPipe } from '@tapiz/cdk/pipes/safe-html';
 import { BoardActions } from '../../actions/board.actions';
-import { MatDialogRef } from '@angular/material/dialog';
 import { NodesActions } from '@tapiz/nodes/services/nodes-actions';
 import { pageFeature } from '../../reducers/page.reducer';
-
-interface State {
-  tags: CocomaterialTag[];
-  allTags: CocomaterialTag[];
-  filteredTags: CocomaterialTag[];
-  vectors: CocomaterialApiVector[];
-}
+import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
+import { explicitEffect } from 'ngxtension/explicit-effect';
+import { BoardMoveService } from '../../services/board-move.service';
 
 @Component({
   selector: 'tapiz-cocomaterial',
@@ -57,86 +60,94 @@ interface State {
   providers: [RxState],
 })
 export class CocomaterialComponent {
-  private state = inject<RxState<State>>(RxState<State>);
-  private store = inject(Store);
-  public dialogRef = inject<MatDialogRef<CocomaterialComponent>>(
-    MatDialogRef<CocomaterialComponent>,
-  );
-  private nodesActions = inject(NodesActions);
-  @ViewChild('tagInput') tagInput!: ElementRef<HTMLInputElement>;
+  #store = inject(Store);
+  #boardMoveService = inject(BoardMoveService);
+  #nodesActions = inject(NodesActions);
+  tagInput = viewChild.required<ElementRef<HTMLInputElement>>('tagInput');
 
-  public readonly viewModel$ = this.state.select();
-  public readonly boardMode = this.store.selectSignal(
-    pageFeature.selectBoardMode,
-  );
+  boardMode = this.#store.selectSignal(pageFeature.selectBoardMode);
 
-  public separatorKeysCodes: number[] = [ENTER, COMMA];
+  tags = signal<CocomaterialTag[]>([]);
+  filteredTags!: Signal<CocomaterialTag[]>;
+
+  cocomaterial = this.#store.selectSignal(selectCocomaterial);
+  allTags = computed(() => this.cocomaterial().tags);
+  vectors = computed(() => this.cocomaterial().vectors?.results ?? []);
+  selected = signal<CocomaterialApiVector | null>(null);
+
+  separatorKeysCodes: number[] = [ENTER, COMMA];
 
   constructor() {
-    this.state.set({
-      tags: [],
-      filteredTags: [],
-    });
-
-    this.state.connect(
-      'allTags',
-      this.store.select(selectCocomaterial).pipe(map((cc) => cc.tags)),
-    );
-
-    this.state.connect(
-      'vectors',
-      this.store
-        .select(selectCocomaterial)
-        .pipe(map((cc) => cc.vectors?.results ?? [])),
-    );
-
     const search = this.form.get('search');
 
     if (search) {
-      this.state.connect(
-        'filteredTags',
-        search.valueChanges.pipe(
-          startWith(null),
-          map((tag: string | null) => {
-            return tag ? this.filter(tag) : this.state.get('allTags');
-          }),
-        ),
+      const search$ = search.valueChanges.pipe(
+        startWith(null),
+        map((tag: string | null) => {
+          return tag ? this.#filter(tag) : this.allTags();
+        }),
       );
+
+      this.filteredTags = toSignal(search$, {
+        initialValue: [],
+      });
     }
 
-    this.state.hold(
-      this.state.select('tags').pipe(
-        map((tags) => {
-          return tags.map((tag) => tag.slug);
-        }),
-      ),
-      (tags) => {
-        this.store.dispatch(PageActions.fetchVectors({ tags }));
-      },
-    );
+    explicitEffect([this.tags], ([tags]) => {
+      const tagSlugs = tags.map((tag) => tag.slug);
+
+      this.#store.dispatch(PageActions.fetchVectors({ tags: tagSlugs }));
+    });
+
+    explicitEffect([this.selected], ([selected]) => {
+      if (selected) {
+        this.#store.dispatch(
+          PageActions.setBoardCursor({ cursor: 'crosshair' }),
+        );
+      } else {
+        this.#store.dispatch(PageActions.setBoardCursor({ cursor: 'default' }));
+      }
+    });
+
+    this.#boardMoveService
+      .relativeMouseDown()
+      .pipe(
+        takeUntilDestroyed(),
+        filter(() => !!this.selected()),
+      )
+      .subscribe((data) => {
+        this.#addVectorToBoard(data);
+      });
   }
 
-  public form = new FormGroup({
+  form = new FormGroup({
     search: new FormControl(''),
   });
 
-  public add(event: MatChipInputEvent): void {
+  toggleSelect(vector: CocomaterialApiVector) {
+    if (this.selected() === vector) {
+      this.selected.set(null);
+      return;
+    }
+
+    this.selected.set(vector);
+  }
+
+  add(event: MatChipInputEvent): void {
     const value = (event.value || '').trim();
-    const added = this.state.get('tags').find((it) => it.slug === value);
+    const added = this.tags().find((it) => it.slug === value);
 
     if (added) {
       return;
     }
 
-    this.state.set('tags', (state) => {
-      const tag = state.allTags.find((tag) => tag.slug === value);
+    const tag = this.allTags().find((tag) => tag.slug === value);
 
-      if (tag) {
-        return [...state.tags, tag];
-      }
-
-      return state.tags;
-    });
+    if (tag) {
+      this.tags.update((tags) => {
+        return [...tags, tag];
+      });
+    }
 
     if (event.chipInput) {
       event.chipInput.clear();
@@ -144,72 +155,64 @@ export class CocomaterialComponent {
     this.form.get('search')?.setValue(null);
   }
 
-  public remove(removeTag: CocomaterialTag): void {
-    this.state.set('tags', (state) => {
-      return state.tags.filter((tag) => {
+  remove(removeTag: CocomaterialTag): void {
+    this.tags.update((tags) => {
+      return tags.filter((tag) => {
         return tag.slug !== removeTag.slug;
       });
     });
   }
 
-  public selected(event: MatAutocompleteSelectedEvent): void {
-    this.state.set('tags', (state) => {
-      const tag = state.allTags.find(
+  optionSelected(event: MatAutocompleteSelectedEvent): void {
+    this.tags.update((tags) => {
+      const tag = this.allTags().find(
         (tag) => tag.slug === event.option.viewValue,
       );
 
       if (tag) {
-        return [...state.tags, tag];
+        return [...tags, tag];
       }
 
-      return state.tags;
+      return tags;
     });
 
-    this.tagInput.nativeElement.value = '';
+    this.tagInput().nativeElement.value = '';
     this.form.get('search')?.setValue(null);
   }
 
-  public selectVector(vector: CocomaterialApiVector, event: MouseEvent) {
-    this.store
-      .select(selectPosition)
-      .pipe(withLatestFrom(this.store.select(selectZoom)), take(1))
-      .subscribe(([position, zoom]) => {
-        if (vector.svg || vector.gif) {
-          this.store.dispatch(
-            BoardActions.batchNodeActions({
-              history: true,
-              actions: [
-                this.nodesActions.add(vector.svg ? 'vector' : 'image', {
-                  url: vector.svg ?? vector.gif ?? '',
-                  width: 150,
-                  height: 150,
-                  position: {
-                    x: (-position.x + event.pageX) / zoom - 75,
-                    y: (-position.y + event.pageY) / zoom - 75,
-                  },
-                  rotation: 0,
-                  layer: this.boardMode(),
-                }),
-              ],
+  onScroll() {
+    const tags = this.tags().map((tag) => tag.slug);
+
+    this.#store.dispatch(PageActions.nextVectorsPage({ tags }));
+  }
+
+  #addVectorToBoard(data: { layer: number; position: Point }) {
+    const vector = this.selected();
+
+    if (vector && (vector.svg || vector.gif)) {
+      this.#store.dispatch(
+        BoardActions.batchNodeActions({
+          history: true,
+          actions: [
+            this.#nodesActions.add(vector.svg ? 'vector' : 'image', {
+              url: vector.svg ?? vector.gif ?? '',
+              width: 150,
+              height: 150,
+              position: data.position,
+              rotation: 0,
+              layer: data.layer,
             }),
-          );
-        }
-      });
-
-    this.dialogRef.close();
+          ],
+        }),
+      );
+    }
   }
 
-  public onScroll() {
-    const tags = this.state.get('tags').map((tag) => tag.slug);
-
-    this.store.dispatch(PageActions.nextVectorsPage({ tags }));
-  }
-
-  private filter(value: string): CocomaterialTag[] {
+  #filter(value: string): CocomaterialTag[] {
     const filterValue = value.toLowerCase();
 
-    return this.state
-      .get('allTags')
-      .filter((tag) => tag.name.toLowerCase().includes(filterValue));
+    return this.allTags().filter((tag) =>
+      tag.name.toLowerCase().includes(filterValue),
+    );
   }
 }
