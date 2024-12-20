588f9d6a8709285109dabb43a7b3a6944660eebe
Juanfran <juanfran.ag@gmail.com>
2024-12-20 09:29:08 +0100

    feat: new rebuild docker script

--- scripts/rebuild-docker.sh
+++ scripts/rebuild-docker.sh
@@ -1,14 +1,37 @@
 #!/bin/bash
 
-rm -rf dist/
-
 WEB_IMAGE="tapiz-web"
 API_IMAGE="tapiz-api"
 
-docker-compose down
+function rebuild_web() {
+    echo "Rebuilding and starting only the web service..."
+    docker-compose down
+    docker rmi $WEB_IMAGE
+    docker-compose build web
+    docker-compose up -d
+}
 
-docker rmi $WEB_IMAGE $API_IMAGE
+function rebuild_api() {
+    echo "Rebuilding and starting only the API service..."
+    docker-compose down
+    docker rmi $API_IMAGE
+    docker-compose build api
+    docker-compose up -d
+}
 
-docker-compose build
+function rebuild_all() {
+    echo "Rebuilding and starting both web and API services..."
+    docker-compose down
+    docker rmi $WEB_IMAGE $API_IMAGE
+    docker-compose build
+    docker-compose up -d
+}
 
-docker-compose up -d
+# Parse command line arguments
+if [[ $1 == "--web" ]]; then
+    rebuild_web
+elif [[ $1 == "--api" ]]; then
+    rebuild_api
+else
+    rebuild_all
+fi
