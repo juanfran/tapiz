e641353b3aa752355a5a09d4034d3ff9fe58edbc
Juanfran <juanfran.ag@gmail.com>
2025-01-28 20:57:40 +0100

    feat: resize multiple & keep aspect ratio

--- libs/ui/src/lib/resize/resize-handler.component.ts
+++ libs/ui/src/lib/resize/resize-handler.component.ts
@@ -9,9 +9,9 @@ import {
 import { MoveService } from '@tapiz/cdk/services/move.service';
 import { Resizable, ResizePosition, TuNode } from '@tapiz/board-commons';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { filterNil } from 'ngxtension/filter-nil';
 import { ResizeService } from './resize.service';
 import { input } from '@angular/core';
+import { ResizeEvent } from './resize.model';
 
 @Component({
   selector: 'tapiz-resize-handler',
@@ -87,7 +87,7 @@ import { input } from '@angular/core';
     `,
   ],
 })
-export class ResizeHandlerComponent implements Resizable {
+export class ResizeHandlerComponent {
   private moveService = inject(MoveService);
   private destroyRef = inject(DestroyRef);
   private resizeService = inject(ResizeService);
@@ -96,28 +96,12 @@ export class ResizeHandlerComponent implements Resizable {
 
   public scale = input(1);
 
-  public get nodeType() {
-    return this.node().type;
-  }
-
   public get width() {
     return this.node().content.width;
   }
 
   public get height() {
     return this.node().content.height;
-  }
-
-  public get position() {
-    return this.node().content.position;
-  }
-
-  public get rotation() {
-    return this.node().content.rotation;
-  }
-
-  public get id() {
-    return this.node().id;
   }
 
   @ViewChild('topLeft') public set topLeft(el: ElementRef<HTMLElement>) {
@@ -139,33 +123,15 @@ export class ResizeHandlerComponent implements Resizable {
   }
 
   public listen(handler: HTMLElement, position: ResizePosition) {
-    const onStart = () => {
-      this.resizeService.startEvent.next({
-        type: this.nodeType,
-        id: this.id,
-        content: {
-          width: this.width,
-          height: this.height,
-        },
-      });
-    };
-
     this.moveService
-      .listenIncrementalAreaSelector(handler, this, position, onStart)
-      .pipe(filterNil(), takeUntilDestroyed(this.destroyRef))
-      .subscribe((size) => {
+      .mouseDownAndMove(handler)
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((mouseEvent) => {
         this.resizeService.resizeEvent.next({
-          type: this.nodeType,
-          id: this.id,
-          content: {
-            position: {
-              x: size.x,
-              y: size.y,
-            },
-            width: Math.round(size.width),
-            height: Math.round(size.height),
-          },
-        });
+          ...mouseEvent,
+          position,
+          nodeRotation: this.node().content.rotation,
+        } as ResizeEvent);
       });
   }
 }
